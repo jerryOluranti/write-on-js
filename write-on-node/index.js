000f09b05c8850000e33c6ecf3038f81be391d7c
Jeremiah Faluyi <faluyijeremiah2018@gmail.com>
2022-01-08 12:19:44 +0100

    refactored for node

--- write-on-node/index.js
+++ write-on-node/index.js
@@ -45,33 +45,43 @@ const erase = async (setstate, text, delayTime) => {
     return;
 }
 
-const writeOn = async (setstate, text = null, list = null, loopTimes = null, delayTime = 0.5, waitTime = 1) => {
-    
-    //pointer variable
-    let i = 0;
+/**
+ * 
+ * @param {setstate, text, list, loopTimes, delayTime, waitTime} params 
+ */
+const writeOn = async ({
+  setstate,
+  text,
+  list,
+  loopTimes,
+  delayTime = 0.5,
+  waitTime = 1,
+}) => {
+  //pointer variable
+  let i = 0;
 
-    //check if a Text was passed in
-    if (text) {
-        while (true) {
-            if (loopTimes && i === loopTimes - 1) break;
-            await write(setstate, text, delayTime, waitTime);
-            i++;
-        }
+  //check if a Text was passed in
+  if (text) {
+    while (true) {
+      if (loopTimes && i === loopTimes - 1) break;
+      await write(setstate, text, delayTime, waitTime);
+      i++;
     }
-    
-    //check if an Array was passed in
-    if (list) {
-        let j = 0;
-        while (true) {
-            if (loopTimes && i === loopTimes) break;
-            await write(setstate, list[j], delayTime, waitTime);
-            j++;
-            if (j === list.length){
-                j = 0;
-                i++;
-            }
-        }
+  }
+
+  //check if an Array was passed in
+  if (list) {
+    let j = 0;
+    while (true) {
+      if (loopTimes && i === loopTimes) break;
+      await write(setstate, list[j], delayTime, waitTime);
+      j++;
+      if (j === list.length) {
+        j = 0;
+        i++;
+      }
     }
-}
+  }
+};
 
 export default writeOn;
